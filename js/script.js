e1e5bd9ac355c9e516d2a627f32216f52c7c5999
Rafael <rafaelribeiroramo@gmail.com>
2024-07-12 01:05:11 -0300

    12/07/2024

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function redirecionar(){
     console.log('Função chamada');
     window.location.href = 'produtos.html';
 }
+//navigation auto
+let counter = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    proximagem();
+}, 5000)
+
+function proximagem(){
+    counter++;
+    if(counter>3){
+        counter = 1;
+    }
+
+    document.getElementById("radio"+counter).checked = true;
+}
